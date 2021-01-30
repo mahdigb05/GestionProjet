3f587a9541f1eb2b6389a76118739b978c7c735d
mahdigb05 <mahdibenmoumen@gmail.com>
2021-01-30 16:56:38 +0100

    added rest controller

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -13,8 +13,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import services.GpUserDetailService;
 
+@RestController
 public class LoginController {
 
     @Autowired
